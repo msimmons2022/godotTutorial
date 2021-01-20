c900c94042f54ce2568624cef5b0a972bcb68764
Draethius <54371536+Draethius@users.noreply.github.com>
2021-01-20 11:56:24 -0600

    Not much
    Followed along on zoom and fiddled with things
    fixed my character invisibility by deleting hide()

--- Player.gd
+++ Player.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -13,8 +12,9 @@ signal hit
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2() # The player's movement vector
 	
