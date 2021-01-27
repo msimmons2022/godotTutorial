a19e3037296e7d6082ccfd606328f07b99a3ad9d
Draethius <54371536+Draethius@users.noreply.github.com>
2021-01-27 10:52:06 -0600

    finished hud and fixed mob animation

--- Main.gd
+++ Main.gd
@@ -7,7 +7,7 @@ var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	new_game()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,11 +17,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -29,6 +32,8 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
+
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D
