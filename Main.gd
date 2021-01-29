be20ad7137a6d44ca3effa8e416270df589cbb96
Draethius <54371536+Draethius@users.noreply.github.com>
2021-01-29 10:44:50 -0600

    Finished Tutorial

--- Main.gd
+++ Main.gd
@@ -18,6 +18,9 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -25,6 +28,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
